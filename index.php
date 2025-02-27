d030d5f08550694c005d808f93d9922caff10ac6
Reckordp <rmuhammad134@gmail.com>
2025-02-27 14:27:57 +0700

    Add Header

--- index.php
+++ index.php
@@ -4,8 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snailight Inc</title>
+    <link rel="stylesheet" href="assets/css/header.css">
 </head>
 <body>
+    <div class="header">
+        <a href="/">
+            <span>
+                <img src="assets/img/logo.png" alt="Coorporation">
+                <span>Snailight</span>
+            </span>
+        </a>
+        <a href="#navigation">
+            <span></span><span></span><span></span>
+        </a>
+    </div>
 <?php
     echo('<h1>Hello World</h1>')
 ?>
