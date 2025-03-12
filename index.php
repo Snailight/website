e080d2bb66398089ba7a2dd720a10904682e8a38
Reckordp <rmuhammad134@gmail.com>
2025-03-12 12:16:25 +0700

    Change index.php

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snailight</title>
+    <link rel="stylesheet" href="assets/vendor/bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/vendor/bootstrap-icons/bootstrap-icons.min.css">
     <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/page.css">
 </head>
 <body>
     <div class="header">
