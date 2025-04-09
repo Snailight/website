e16285e03eca2d10858ab9377d845395cbfece3c
Reckordp <rmuhammad134@gmail.com>
2025-04-09 20:22:54 +0700

    Fix cookie with domain

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', e => {
     document.getElementById('akun-logout').addEventListener('click', e => {
         e.preventDefault();
-        document.cookie = 'userId=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/'
+        document.cookie = 'userId=; domain=' + window.location.hostname + '; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/'
         window.location = '/user/dashboard.php';
     });
 });
